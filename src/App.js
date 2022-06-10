869a57d2e4f71b8be854caa6a13ba2afa7ed255c
Matheus Albuquerque <matheusgmcm@gmail.com>
2022-06-09 22:07:11 -0300

    Aulas_React

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{useState, useEffect} from "react";
+import { ComponentePai } from "./Pages/ComponentePai";
+import {IndiretaPai} from "./Pages/IndiretaPai"
+import {Input} from "./Pages/Input"
 
 function App() {
+
+  const[count, setCount] = useState(0)
+
+  useEffect(()=>{
+    document.title = `Voce clicou ${count} vezes`
+  },[count])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {/* <ComponentePai/> */}
+      {/* <IndiretaPai/> */}
+      <Input/>
+    </>
   );
 }
 
