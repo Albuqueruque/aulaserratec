582c5bbcae655185d1838325b92901d6cf4664ab
Matheus Albuquerque <matheusgmcm@gmail.com>
2022-06-10 22:03:35 -0300

    React-Router-add

--- src/App.js
+++ src/App.js
@@ -1,26 +1,12 @@
 import React,{useState, useEffect} from "react";
-import { ComponentePai } from "./Pages/ComponentePai";
-import {IndiretaPai} from "./Pages/IndiretaPai"
-import {Input} from "./Pages/Input"
-import {AdicionarLista} from "./Pages/AdicionarLista"
-import {RenderizacaoCondicional} from "./Pages/RenderizacaoCondicional"
-
+import { Root } from "./Routes/root";
 
 function App() {
 
-  const[count, setCount] = useState(0)
-
-  // useEffect(()=>{
-  //   document.title = `Voce clicou ${count} vezes`
-  // },[count])
 
   return (
     <>
-      {/* <ComponentePai/> */}
-      {/* <IndiretaPai/> */}
-      {/* <Input/> */}
-      {/* <AdicionarLista/> */}
-      <RenderizacaoCondicional/>
+      <Root/>
     </>
   );
 }
