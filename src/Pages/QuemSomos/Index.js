4247ba306b36046af945f3573f3c2ea0ae37b80c
Matheus Albuquerque <matheusgmcm@gmail.com>
2022-06-14 19:52:52 -0300

    Axios_get

--- src/Pages/QuemSomos/Index.js
+++ src/Pages/QuemSomos/Index.js
@@ -1,10 +1,26 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import {FirstText, SecondText, Container, AulaButton} from "./style.js"
 import {Input} from "../../Components/Input.js"
 import {AdicionarLista} from "../../Components/AdicionarLista.js"
+import { api } from "../../Services/api.js";
 
-export const QuemSomos = () =>{
+export const QuemSomos = (props) =>{
+
+    const [produto, setProduto] = useState({
+        nome:"",
+        preco: ""
+    })
+
+
+    useEffect(()=>{
+        const getUsuario = async () =>{
+            const response = await api.get(`/allproducts`)
+            setProduto(response.data)
+        }
+        getUsuario()
+    },[])
+
 
     var {nome} = useParams()
     var {idade} = useParams()
